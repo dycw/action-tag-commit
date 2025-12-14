6d26fef3a319822ba423976ce731aaeb9acd638e
Derek Wan <d.wan@icloud.com>
2025-12-15 04:52:45 +0900

    Extend to full range of tags (#15)

--- script.py
+++ script.py
@@ -10,8 +10,9 @@
 # ///
 from __future__ import annotations
 
+from contextlib import suppress
 from logging import getLogger
-from subprocess import check_call, check_output
+from subprocess import CalledProcessError, check_call, check_output
 
 from click import command
 from typed_settings import click_options, option, settings
@@ -33,17 +34,39 @@ def main(settings: Settings, /) -> None:
     if settings.dry_run:
         _LOGGER.info("Dry run; exiting...")
         return
-    cmds1 = ["bump-my-version", "show", "current_version"]
+    cmds = ["bump-my-version", "show", "current_version"]
+    _LOGGER.info("Running '%s'...", " ".join(cmds))
+    version = parse_version(check_output(cmds, text=True).rstrip("\n"))
+    _tag(str(version))
+    _tag(f"{version.major}.{version.minor}")
+    _tag(str(version.major))
+    _tag("latest")
+    _LOGGER.info("Finished")
+
+
+def _tag(version: str, /) -> None:
+    _delete_tag(version)
+    _add_tag(version)
+
+
+def _delete_tag(version: str, /) -> None:
+    cmds1 = ["git", "tag", "--delete", version]
     _LOGGER.info("Running '%s'...", " ".join(cmds1))
-    version = parse_version(check_output(cmds1, text=True).rstrip("\n"))
-    _LOGGER.info("Current version is %s", version)
-    cmds2 = ["git", "tag", "-a", str(version), "HEAD", "-m", str(version)]
+    with suppress(CalledProcessError):
+        _ = check_call(cmds1)
+    cmds2 = ["git", "push", "--delete", "origin", version]
+    _LOGGER.info("Running '%s'...", " ".join(cmds2))
+    with suppress(CalledProcessError):
+        _ = check_call(cmds2)
+
+
+def _add_tag(version: str, /) -> None:
+    cmds1 = ["git", "tag", "-a", version, "HEAD", "-m", version]
+    _LOGGER.info("Running '%s'...", " ".join(cmds1))
+    _ = check_call(cmds1)
+    cmds2 = ["git", "push", "--tags", "--force", "--set-upstream", "origin"]
     _LOGGER.info("Running '%s'...", " ".join(cmds2))
     _ = check_call(cmds2)
-    cmds3 = ["git", "push", "--tags", "--force", "--set-upstream", "origin"]
-    _LOGGER.info("Running '%s'...", " ".join(cmds3))
-    _ = check_call(cmds3)
-    _LOGGER.info("Finished")
 
 
 if __name__ == "__main__":
