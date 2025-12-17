fcc43911fb265fdf1e1cdc5b122ba46b7632d2f2
Derek Wan <d.wan@icloud.com>
2025-12-17 16:09:06 +0900

    Add `APP_TOKEN` to settings (#37)

--- src/tag_commit/settings.py
+++ src/tag_commit/settings.py
@@ -5,6 +5,7 @@ from typed_settings import option, settings
 
 @settings
 class Settings:
+    token: str = option(default="token", help="GitHub token")
     user_name: str = option(default="github-actions-bot", help="'git' user name")
     user_email: str = option(default="noreply@github.com", help="'git' user email")
     major_minor: bool = option(default=False, help="Add the 'major.minor' tag")
