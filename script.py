d728b63eedd256bc14acd0bae939ad6c9ecfe076
Derek Wan <d.wan@icloud.com>
2025-12-17 13:53:27 +0900

    Make `APP_TOKEN` an env var (#28)

--- script.py
+++ script.py
@@ -29,6 +29,7 @@ _LOGGER = getLogger(__name__)
 
 @settings
 class Settings:
+    token: str = option(default="token", help="GitHub token")
     user_name: str = option(default="github-actions-bot", help="'git' user name")
     user_email: str = option(default="noreply@github.com", help="'git' user email")
     major_minor: bool = option(default=False, help="Add the 'major.minor' tag")
